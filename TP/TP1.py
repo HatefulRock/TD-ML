44dda20e545a320a8b0c3d6fe7467cc0f5053995
Pierre Blachere <pierre.blachere@outlook.com>
2022-09-28 08:35:33 +0200

    works but with weird errors

--- TP/TP1.py
+++ TP/TP1.py
@@ -12,7 +12,7 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.nins=nins
         self.nout=nout
-        nhid = int((nins+nout)/2)
+        nhid = 2
         self.hidden = nn.Linear(nins, nhid)
         self.out    = nn.Linear(nhid, nout)
 
@@ -23,53 +23,39 @@ class Net(nn.Module):
 
 
 
-def test(model, data, target):
-    x=torch.FloatTensor(data)
-    y=model(x).data.numpy()
-    haty = np.argmax(y,axis=1)
-    nok=sum([1 for i in range(len(target)) if target[i]==haty[i]])
-    acc=float(nok)/float(len(target))
-    return acc
+# def test(model, data, target):
+#     x=torch.FloatTensor(data)
+#     y=model(x).data.numpy()
+#     haty = np.argmax(y,axis=0)
+#     nok=sum([1 for i in range(len(target)) if target[i]==haty[i]])
+#     acc=float(nok)/float(len(target))
+#     return acc
 
 def train(model, data, target):
     optim = torch.optim.Adam(model.parameters(), lr=0.001)
     criterion = nn.CrossEntropyLoss()
 
     x=torch.FloatTensor(data)
-    y=torch.LongTensor(target)
+    y=torch.FloatTensor(target)
+    print(x)
+    print(y)
     for epoch in range(100):
-        #i=np.arange(len(x))
-        #np.random.shuffle(i)
         epoch_loss=[]
         optim.zero_grad()
         haty = model(x)
+        print(haty)
         loss = criterion(haty,y)
         epoch_loss.append(loss.item())
-        acc = test(model, data, target)
-        print(str(loss.item())+" "+str(acc))
+        #acc = test(model, data, target)
+        #print(str(loss.item())+" "+str(acc))
         loss.backward()
+
         optim.step()
         loss_vals.append(sum(epoch_loss)/len(x))
-        #idx = np.arange(len(x))
     
 
-# def genData(nins, nsamps):
-#     prior0 = 0.7
-#     mean0  = 0.3
-#     var0   = 0.1
-#     mean1  = 0.8
-#     var1   = 0.01
-
-#     n0 = int(nsamps*prior0)
-#     x0=var0 * np.random.randn(n0,nins) + mean0
-#     x1=var1 * np.random.randn(nsamps-n0,nins) + mean1
-#     x = np.concatenate((x0,x1), axis=0)
-#     y = np.ones((nsamps,),dtype='int64')
-#     y[:n0] = 0
-#     return x,y
-
 def func(x,y):
-    return m.exp(x+y)
+    return m.sin(x+y)
 
 
 def DataGenerator(number):
@@ -81,10 +67,13 @@ def DataGenerator(number):
 
 
 def toytest():
-    model = Net(2,1)
+    model = Net(20,20)
     data=DataGenerator(20)
-    inputdata=[[data[i][0],data[i][1]] for i in range(len(data))]
+    #inputdata=[[data[i][0],data[i][1]] for i in range(len(data))]
+    inputdata=[data[i][0] for i in range(len(data))]
+
     target=[data[i][2] for i in range(len(data))]
+    print(target)
     train(model,inputdata,target)
 
 
