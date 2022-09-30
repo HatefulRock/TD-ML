df8cc99f29d710d2750e7844c7122b156ad450f2
Pierre Blachere <pierre.blachere@outlook.com>
2022-09-30 19:50:21 +0200

    various changes to code and readme

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # TD-ML
 
-La fonction que nous avons choisi est f(u,v)=
 
-Résultats:
+La fonction que nous avons choisie est f(u,v)=cos(x+y)
 
-Changements d'hyperparametres:
-Si on change le learning rate, le nomre de couches cachées, l'optimiseur, quelle est l'influence sur les résultats
+Le reseau qu'on a cherché à impémenter pour résoudre ce problème de régression est un réseau feed-forward a deux entrées , 10 couches chachées et une sortie. La fonction d'activation utilisée est tanh.
+L'optimiseur utilisé est Adam, une fonction qui donne des bons résultats et qui converge rapidement. Le nombre d'epochs est 500.
+Pour calculer la loss, on utilise comme métrique la Mean Squared Error qui est une métrique adaptée pour les problèmes de régression. Elles est definie comme etant sqrt(mean((y-ypred)**2)) avec y la valeur qu'on cherche à prédire et ypred la valeur prédite.
+
+##Données
+
+Pour créer les données d'entrée du réseau on crée un array et on l'initialise avec des valeurs aléatoirement distribuées dans l'intervalle [-5,5]. Le nombre de valeurs est un paramètre modifiable. Ensuite, on crée les valeurs target comme étant les images par la fonction f des valeurs d'entrée. Ces valeurs la sont donc les valeurs que le réseau va chercher a apprendre et calculer.
+
+
+
+##Résultats:
+
+Les résultats ne semblent pas etre très concluants comme on peut voir avec les graphiques ci dessous:
+
+![Training and validation loss](/home/hatefulrock/Pictures/loss.png'  "Training and validation loss")
+![Training and validation accuracy](/home/hatefulrock/Pictures/acc.png'  "Training and validation accuracy")
+
--- TP/TP1.py
+++ TP/TP1.py
@@ -13,7 +13,7 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.nins=nins
         self.nout=nout
-        nhid = 5
+        nhid = 10
         self.hidden = nn.Linear(nins, nhid)
         self.out    = nn.Linear(nhid, nout)
 
@@ -24,29 +24,16 @@ class Net(nn.Module):
 
 
 
-# def test(model, data, target):
-#     x=torch.FloatTensor(data)
-#     y=model(x).detach().numpy()
-#     haty = np.argmax(y,axis=1)
-#     nok=sum([1 for i in range(len(target)) if target[i]==haty[i]])
-#     acc=float(nok)/float(len(target))
-#     return acc
-
 def test(model, data, target):
     #implement mse accuracy formula
     x=torch.FloatTensor(data)
     y=model(x).detach().numpy()
+    #print(y)
     ypred=torch.FloatTensor(target).detach().numpy()
-
+    #print(ypred)
     acc=np.sqrt(np.mean((y-ypred)**2))
+    
     return acc
-
-
-# def test(model, data, target):
-#     x=torch.FloatTensor(data)
-#     y=np.squeeze(model(x).data.numpy())
-#     return np.sqrt(np.mean((np.squeeze(target)-y)**2))
-
 
 
 def train(model, data, target):
@@ -71,75 +58,43 @@ def train(model, data, target):
 
     #draw 2d graph of network output
     plt.plot(y, label='y')
+    plt.legend()
+    plt.show()
+
+    #draw plot of haty
     plt.plot(haty, label='haty')
     plt.legend()
     plt.show()
 
-
-    # #draw 3d graph of output of network
-    # fig=plt.figure()
-    # ax=plt.axes(projection='3d')
-    # n_point=100
-    # ax.plot(x[:n_point,0], x[:n_point,1], haty[:n_point], 'b', markersize=0.5)
-    # plt.show()
     
 
 def func(x,y):
     return np.cos(x+y)
 
 
-def DataGenerator(number):
-    data=[]
-    x=np.linspace(-number,number,100)
-    for j in range(len(x)):
-        data.append([j,j,func(j,j)])
-    return data
-
+def DataGenerator(number_samples):
+    #initialize training data with random values between -5 and 5
+    train_data=np.random.uniform(-5,5,(number_samples,2))
+    #print(train_data)
+    test_data=func(train_data[:,0],train_data[:,1])
+    test_data = np.expand_dims(test_data, axis=1)
+    print(test_data)
+    return train_data,test_data
 
 def toytest():
     model = Net(2,1)
-    data=DataGenerator(5)
+    training_data,training_target=DataGenerator(100)
     #inputdata=[[data[i][0],data[i][1]] for i in range(len(data))]
 
-    #input create array of dimensions 20x2
-    inputdata=np.array([[data[i][0],data[i][1]] for i in range(len(data))])
-    #inputdata=[data[i][0] for i in range(len(data))]
-    #print(inputdata)
+    #target=np.array([[data[i][2]] for i in range(len(data))])
+    train(model,training_data,training_target)
 
-    target=np.array([[data[i][2]] for i in range(len(data))])
-    train(model,inputdata,target)
-
-    # #draw 3d graph of output of network
-    # x=np.linspace(-5,5,100)
-    # y=np.linspace(-5,5,100)
-    # Z=np.transpose(model(torch.FloatTensor(inputdata)).detach().numpy()).tolist()[0]
-    # print(Z)
-
-    # fig = plt.figure()
-    # ax = plt.axes(projection='3d')
-    # ax.contour3D(x, y, Z, 50, cmap='binary')
-    # ax.set_xlabel('x')
-    # ax.set_ylabel('y')
-    # ax.set_zlabel('z')
     
 
 
 
 if __name__ == "__main__":
     toytest()
-
-
-# x = np.linspace(-6, 6, 30)
-# y = np.linspace(-6, 6, 30)
-
-# X, Y = np.meshgrid(x, y)
-# Z = func(X, Y)
-# print(X)
-
-# print(Z)
-
-
-
 
 
 
