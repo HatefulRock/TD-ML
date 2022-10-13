810b80487a2158279ed99ac8b7ab61c9e8f4ab49
Pierre Blachere <pierre.blachere@outlook.com>
2022-10-13 19:24:21 +0200

    rnn time series

--- TP/TP2/tp2.py
+++ TP/TP2/tp2.py
@@ -1,35 +1,49 @@
-# overfitting
-
-import sys
-import torch.nn as nn
-import torch
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+from torch.autograd import Variable
 
-dn = 50.
+
 h=10
 nepochs=100
+dn=50.
 
-with open("2019.csv","r") as f: ls=f.readlines()
-trainx = torch.Tensor([float(l.split(',')[1])/dn for l in ls[:-1]]).view(1,-1,1)
-trainy = torch.Tensor([float(l.split(',')[1])/dn for l in ls[1:]]).view(1,-1,1)
-with open("2020.csv","r") as f: ls=f.readlines()
-testx = torch.Tensor([float(l.split(',')[1])/dn for l in ls[:-1]]).view(1,-1,1)
-testy = torch.Tensor([float(l.split(',')[1])/dn for l in ls[1:]]).view(1,-1,1)
+#import data from meteo 2019
+testing_data = pd.read_csv('2020.csv', sep=',',header=None)
+training_data = pd.read_csv('2019.csv', sep=',', header=None)
 
-# scaler = MinMaxScaler(feature_range=(-1, 1))
-# trainxn = scaler.fit_transform(trainx .reshape(-1, 1))
-# trainyn = scaler.fit_transform(trainy .reshape(-1, 1))
+#data description
+#print(train_data.describe())
+#print(train_data.head())
+
+all_data_train = training_data[1].values.astype(float)
+all_data_test = testing_data[1].values.astype(float)
+#print(all_data)
+
+trainx= all_data_train[:-7]/dn
+trainy= all_data_train[7:]/dn
+testx= all_data_test[:-7]/dn
+testy= all_data_test[7:]/dn
 
 
 
-# trainx = 1, seqlen, 1
-# trainy = 1, seqlen, 1
-trainds = torch.utils.data.TensorDataset(trainx, trainy)
+trainxn= torch.FloatTensor(trainx).view(1,-1,1)
+trainyn= torch.FloatTensor(trainy).view(1,-1,1)
+testxn= torch.FloatTensor(testx).view(1,-1,1)
+testyn= torch.FloatTensor(testy).view(1,-1,1)
+
+
+# # # trainx = 1, seqlen, 1
+# # # trainy = 1, seqlen, 1
+trainds = torch.utils.data.TensorDataset(trainxn, trainyn)
 trainloader = torch.utils.data.DataLoader(trainds, batch_size=1, shuffle=False)
-testds = torch.utils.data.TensorDataset(testx, testy)
+testds = torch.utils.data.TensorDataset(testxn, testyn)
 testloader = torch.utils.data.DataLoader(testds, batch_size=1, shuffle=False)
 crit = nn.MSELoss()
+
 
 class Mod(nn.Module):
     def __init__(self,nhid):
@@ -75,9 +89,29 @@ def train(mod):
             loss.backward()
             optim.step()
         totloss /= float(nbatch)
-        print("err",totloss,testloss)
+        #print("err",totloss,testloss)
     print("fin",totloss,testloss,file=sys.stderr)
 
 mod=Mod(h)
-print("nparms",sum(p.numel() for p in mod.parameters() if p.requires_grad),file=sys.stderr)
+#print("nparms",sum(p.numel() for p in mod.parameters() if p.requires_grad),file=sys.stderr)
 train(mod)
+
+mod.eval()
+dataX = Variable(torch.Tensor(np.array(trainx)))
+dataY = trainy
+data_predict = mod(trainxn).squeeze(0).data.numpy()*dn
+dataY_plot = dataY
+
+#data_predict = data_predict
+#print(data_predict)
+dataY_plot = dataY_plot*dn
+#print(dataY_plot)
+#length of trainx
+
+
+plt.axvline(x=len(trainx), c='r', linestyle='--')
+
+plt.plot(dataY_plot)
+plt.plot(data_predict)
+plt.suptitle('Time-Series Prediction')
+plt.show()
